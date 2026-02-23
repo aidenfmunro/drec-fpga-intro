d085e20f4187062acc2a14c2629d5467c0a533fb
aidenfmunro <aidenfmunro@gmail.com>
2026-02-24 00:58:36 +0300

    Fix exp issue being +1 more then needed

--- problems/2025/01_fp16/fp16mul.v
+++ problems/2025/01_fp16/fp16mul.v
@@ -4,13 +4,13 @@ module fp16mul
 (
     input  wire [15:0] i_x,
     input  wire [15:0] i_y,
-    output wire [15:0] o_z
+    output reg  [15:0] o_z
 );
 
 wire s_x = i_x[15];
 wire s_y = i_y[15];
 
-wire s_z = s_x[15] ^ s_y[15];
+wire s_z = s_x ^ s_y;
 
 wire [4:0] e_x = i_x [14:10];
 wire [4:0] e_y = i_y [14:10];
@@ -25,15 +25,13 @@ wire [10:0] ext_m_y  = (e_y == 0) ? 11'b0 : {1'b1, m_y};
 wire [21:0] m_prod = ext_m_x * ext_m_y;
 
 wire norm = m_prod[21];
+
 // Use 7 bit signed num to check for (over/under)flow
-
 wire signed [6:0] e_sum = e_x + e_y - 5'b01111;
-// Normalize
 
+// Normalize
 wire [21:0] m_prod_norm = norm ? m_prod >> 1 : m_prod;
 wire signed [6:0] e_sum_norm = e_sum + norm;
-
-wire l, g, t;
 
 wire l = m_prod_norm[10];   // LSB
 wire g = m_prod_norm[9];    // Guard bit
@@ -41,7 +39,7 @@ wire t = |m_prod_norm[8:0]; // Sticky bit
 
 wire rnd_inc = g && (t || l);
 
-wire [10:0] m_round = m_prod_norm[20:10] + rnd_inc;
+wire [10:0] m_round = {1'b0, m_prod_norm[19:10]} + rnd_inc;
 
 always @(*) begin
     // FTZ
@@ -52,13 +50,15 @@ always @(*) begin
         o_z = {s_z, 5'b11111, 10'b0};
     end
     else begin
-        if (m_round[10]) begin
-            o_z = {s_z, e_sum_norm[4:0] + 5'b00001, m_round[9:0]};
-        end
-        else begin
+         if (m_round[10]) begin
+             o_z = {s_z, e_sum_norm[4:0] + 5'b00001, m_round[9:0]};
+         end
+         else begin
             o_z = {s_z, e_sum_norm[4:0], m_round[9:0]};
-        end
+         end
     end
 end
 
 endmodule
+
+// I hope to remember this in years...

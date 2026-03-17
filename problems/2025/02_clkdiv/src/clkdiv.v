70d0097e66c2a47b5b8ad5f06ed116f29e5d8232
aidenfmunro <aidenfmunro@gmail.com>
2026-03-18 00:40:32 +0300

    [02] Fix clkdiv overflow logic

--- problems/2025/02_clkdiv/src/clkdiv.v
+++ problems/2025/02_clkdiv/src/clkdiv.v
@@ -1,6 +1,6 @@
 module clkdiv #(
-    parameter SRC_FREQ  = 50_000_000,
-    parameter DST_FREQ = 12_500_000
+    parameter SRC_FREQ = 50_000_000,
+    parameter DST_FREQ = 9600
 ) (
     input  wire clk,
     input  wire rst_n,
@@ -11,10 +11,12 @@ localparam CNT_WIDTH = $clog2(SRC_FREQ/DST_FREQ);
 
 reg [CNT_WIDTH-1:0] cnt;
 
-assign out = &cnt;
+assign out = (cnt == SRC_FREQ/DST_FREQ);
 
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
+        cnt <= {CNT_WIDTH{1'b0}};
+    end else if (out) begin
         cnt <= {CNT_WIDTH{1'b0}};
     end else begin
         cnt <= cnt + 1'b1;
